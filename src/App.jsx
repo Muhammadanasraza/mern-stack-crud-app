d37af3a7ca14e56941d7aee3050c92d5e161e6f7
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2025-03-05 02:07:31 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
-import Sidebar from "./Components/Sidebar";
-import Services from "./Components/Pages/Service";
-import Contact from "./Components/Pages/Cantact";
 import Products from "./Components/Pages/Products";
 import CreateProduct from "./Components/Pages/CreateProduct";
 
@@ -21,16 +18,12 @@ const App = () => {
         <Header toggleSidebar={toggleSidebar} />
 
         <div className="flex flex-1 pt-16 overflow-hidden">
-          {/* Sidebar */}
-           {/* <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} /> */}
-
+        
           {/* Main Content */}
           <main className="flex-1 p-6 bg-gray-100 overflow-auto ">
             <Routes>
               <Route path="/" element={<Products />} />
               <Route path="/CreateProduct" element={<CreateProduct />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/contact" element={<Contact />} />
             </Routes>
           </main>
         </div>
--- src/Components/Pages/Service.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Services = () => {
-  return <h1 className="text-3xl font-bold">Services Page</h1>;
-};
-
-export default Services;
